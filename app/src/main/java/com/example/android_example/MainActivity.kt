1e974dc5588ae8ba5d2805b390a71817d9e0b9cb
Terry <yungik@glocalizeinc.com>
2022-07-18 19:12:26 +0900

    Modify Main View to WebView

--- app/src/main/java/com/example/android_example/MainActivity.kt
+++ app/src/main/java/com/example/android_example/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.android_example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val myWebView = WebView(applicationContext)
+//        val webview: WebView = findViewById(R.id.webview)
+        myWebView.loadUrl("https://platform.glozinc.com")
+        myWebView.settings.javaScriptEnabled = true
+        setContentView(myWebView)
     }
 }
